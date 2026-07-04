f4b4cd029b94176b7ce81daddaf1d4825a358073
David Wong <davidwong.crypto@gmail.com>
2026-07-04 14:25:42 -0400

    action

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/directmap/',
   plugins: [react()],
   server: {
     allowedHosts: ['host.docker.internal'],
